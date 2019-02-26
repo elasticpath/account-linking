2146587a7a6ab4df6bdf1132b0277ef7ce6a6673
mennis <mark.ennis@elasticpath.com>
2019-02-26 09:30:09 -0800

    Changed sed delimiter to account for URL characters

--- docker/script.sh
+++ docker/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i /auth/src/ep.config.json -e "s/\"scope\": \"vestri\"/\"scope\": \"$SCOPE\"/g"
-sed -i /auth/src/ep.config.json -e "s/\"path\": \"\/cortex\"/\"path\": \"$CORTEX_URL\"/g"
+sed -i /auth/src/ep.config.json -e "s#\"scope\": \"vestri\"#\"scope\": \"$SCOPE\"#g"
+sed -i /auth/src/ep.config.json -e "s#\"path\": \"\/cortex\"#\"path\": \"$CORTEX_URL\"#g"
 
